789ff6a28467c46dd0665b997ad210699b5d296b
Jaswantsuthar <95173617+Jaswantsuthar@users.noreply.github.com>
2024-02-11 13:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function fibonacci(num) {
 // your code here
 	let n1=0,n2=1,n3;
 	for (let i = 0; i <= num; i++) {
-			
-			n3=n1+n2;
 			n1=n2;
 			n2=n3;
 		
+			n3=n1+n2;
+			
 	}
 	return n3;
 }
